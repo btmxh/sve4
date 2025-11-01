f4a352fd0cb836c496c3461fc98bfca5bd7c99a4
btmxh <ngoduyanh.chip@gmail.com>
2025-11-01 16:20:20 +0700

    ci: fix ci 18

--- libsve4_decode/tests/webp.c
+++ libsve4_decode/tests/webp.c
@@ -310,6 +310,6 @@ static const MunitSuite test_suite = {
     MUNIT_SUITE_OPTION_NONE,
 };
 
-int main(int argc, char* argv[MUNIT_ARRAY_PARAM(argc + 1)]) {
+int main(int argc, char* argv[]) {
   return munit_suite_main(&test_suite, NULL, argc, argv);
 }
--- libsve4_utils/tests/allocator.c
+++ libsve4_utils/tests/allocator.c
@@ -114,6 +114,6 @@ static const MunitSuite test_suite = {
     "/allocator", test_suite_tests, NULL, 1, MUNIT_SUITE_OPTION_NONE,
 };
 
-int main(int argc, char* argv[MUNIT_ARRAY_PARAM(argc + 1)]) {
+int main(int argc, char* argv[]) {
   return munit_suite_main(&test_suite, NULL, argc, argv);
 }
