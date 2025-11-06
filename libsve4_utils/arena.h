8b5a344601f6b712968b22d8bd68d7a88f0d2004
btmxh <ngoduyanh.chip@gmail.com>
2025-11-06 19:32:51 +0700

    fix(utils): fix arena issues and warnings

--- libsve4_utils/arena.h
+++ libsve4_utils/arena.h
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 
 #include "allocator.h"
+#include "defines.h"
 
 // ARENA API (currently using tsoding/arena implementation)
 
@@ -12,13 +13,22 @@
       .state = {0},                                                            \
       .alloc = sve4__allocator_arena_alloc,                                    \
       .grow = sve4__allocator_arena_grow,                                      \
+      .free = sve4__allocator_arena_free,                                      \
   })
 
-void* sve4__allocator_arena_alloc(sve4_allocator_t* _Nonnull self, size_t size,
-                                  size_t alignment);
-void* sve4__allocator_arena_grow(sve4_allocator_t* _Nonnull self,
-                                 void* _Nullable ptr, size_t old_size,
-                                 size_t new_size, size_t alignment);
+SVE4_UTILS_EXPORT
+void* _Nullable sve4__allocator_arena_alloc(sve4_allocator_t* _Nonnull self,
+                                            size_t size, size_t alignment);
+SVE4_UTILS_EXPORT
+void* _Nullable sve4__allocator_arena_grow(sve4_allocator_t* _Nonnull self,
+                                           void* _Nullable ptr, size_t old_size,
+                                           size_t new_size, size_t alignment);
+SVE4_UTILS_EXPORT
+void sve4__allocator_arena_free(sve4_allocator_t* _Nonnull self,
+                                void* _Nullable ptr, size_t alignment);
 
+SVE4_UTILS_EXPORT
 void sve4_allocator_arena_destroy(sve4_allocator_t* _Nonnull arena);
+
+SVE4_UTILS_EXPORT
 void sve4_allocator_arena_reset(sve4_allocator_t* _Nonnull arena);
