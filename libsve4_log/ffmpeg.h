79034d5c7a92704b882025c4c7fd7fef1fc0e568
btmxh <ngoduyanh.chip@gmail.com>
2025-11-06 20:18:02 +0700

    fix: add defines header for nullable/nonnull

--- libsve4_log/ffmpeg.h
+++ libsve4_log/ffmpeg.h
@@ -2,5 +2,7 @@
 
 #include <stdio.h>
 
+#include "defines.h"
+
 void sve4_log_ffmpeg_callback(void* _Nullable avcl, int level,
                               const char* _Nonnull fmt, va_list args);
