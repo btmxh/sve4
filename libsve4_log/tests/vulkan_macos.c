96243036cd825e3b881ff6b4c0fa33eb4665e929
btmxh <ngoduyanh.chip@gmail.com>
2025-11-12 17:23:01 +0700

    chore(debug): add debug logging to the debug

--- libsve4_log/tests/vulkan_macos.c
+++ libsve4_log/tests/vulkan_macos.c
@@ -3,6 +3,30 @@
 
 #include "libsve4_utils/volk.h"
 
+// Debug callback for VK_EXT_debug_utils
+static VKAPI_ATTR VkBool32 VKAPI_CALL
+debug_callback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+               VkDebugUtilsMessageTypeFlagsEXT messageTypes,
+               const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+               void* pUserData) {
+
+  (void)messageTypes;
+  (void)pUserData;
+
+  if (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT) {
+    fprintf(stdout, "VERBOSE: %s\n", pCallbackData->pMessage);
+  } else if (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT) {
+    fprintf(stdout, "INFO: %s\n", pCallbackData->pMessage);
+  } else if (messageSeverity &
+             VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT) {
+    fprintf(stderr, "WARNING: %s\n", pCallbackData->pMessage);
+  } else if (messageSeverity & VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT) {
+    fprintf(stderr, "ERROR: %s\n", pCallbackData->pMessage);
+  }
+
+  return VK_FALSE; // Do not abort Vulkan call
+}
+
 int main(void) {
   // Initialize Volk (loader)
   VkResult result = volkInitialize();
@@ -16,21 +40,23 @@ int main(void) {
 
   VkApplicationInfo app_info = {
       .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
-      .pApplicationName = "volk_test",
+      .pApplicationName = "volk_debug_test",
       .applicationVersion = VK_MAKE_VERSION(1, 0, 0),
       .pEngineName = "none",
       .engineVersion = VK_MAKE_VERSION(1, 0, 0),
       .apiVersion = VK_API_VERSION_1_0,
   };
 
+  const char* extensions[] = {VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME,
+                              VK_EXT_DEBUG_UTILS_EXTENSION_NAME};
+
   VkInstanceCreateInfo instance_info = {
       .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
       .pNext = NULL,
       .flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR,
       .pApplicationInfo = &app_info,
-      .enabledExtensionCount = 1,
-      .ppEnabledExtensionNames =
-          (const char*[]){VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME},
+      .enabledExtensionCount = 2,
+      .ppEnabledExtensionNames = extensions,
       .enabledLayerCount = 0,
       .ppEnabledLayerNames = NULL,
   };
@@ -47,12 +73,49 @@ int main(void) {
   volkLoadInstance(instance);
   printf("Loaded Volk instance functions\n");
 
+  // Create debug messenger
+  VkDebugUtilsMessengerCreateInfoEXT debug_info = {
+      .sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+      .messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+                         VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
+                         VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                         VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
+      .messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                     VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                     VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT,
+      .pfnUserCallback = debug_callback,
+      .pUserData = NULL,
+  };
+
+  VkDebugUtilsMessengerEXT debug_messenger;
+  PFN_vkCreateDebugUtilsMessengerEXT fpCreateDebugMessenger =
+      (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+          instance, "vkCreateDebugUtilsMessengerEXT");
+  if (!fpCreateDebugMessenger) {
+    fprintf(stderr, "Failed to load vkCreateDebugUtilsMessengerEXT\n");
+    return 1;
+  }
+
+  result =
+      fpCreateDebugMessenger(instance, &debug_info, NULL, &debug_messenger);
+  if (result != VK_SUCCESS) {
+    fprintf(stderr, "Failed to create debug messenger: %d\n", result);
+    return 1;
+  }
+
+  printf("Debug messenger created\n");
+
+  // Destroy debug messenger
+  PFN_vkDestroyDebugUtilsMessengerEXT fpDestroyDebugMessenger =
+      (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(
+          instance, "vkDestroyDebugUtilsMessengerEXT");
+  fpDestroyDebugMessenger(instance, debug_messenger, NULL);
+  printf("Debug messenger destroyed\n");
+
   // Destroy Vulkan instance
   vkDestroyInstance(instance, NULL);
   printf("Destroyed Vulkan instance: %p\n", (void*)instance);
 
-  // Finalize Volk (optional)
   volkFinalize();
-
   return 0;
 }
