3241714ff168202ddfbd4db4212244af8a701bfd
btmxh <ngoduyanh.chip@gmail.com>
2025-11-12 11:14:44 +0700

    test(vulkan): try to enable debug utils logging

--- libsve4_log/tests/vulkan.c
+++ libsve4_log/tests/vulkan.c
@@ -1,3 +1,5 @@
+#include "libsve4_log/vulkan.h"
+
 #include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -21,6 +23,22 @@ static MunitResult test_vulkan_basic(const MunitParameter params[],
       &(VkInstanceCreateInfo){
           .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
           .flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR,
+          .pNext =
+              &(VkDebugUtilsMessengerCreateInfoEXT){
+                  .sType =
+                      VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+                  .messageSeverity =
+                      VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+                      VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT |
+                      VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
+                      VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT,
+                  .messageType =
+                      VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+                      VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
+                      VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT |
+                      VK_DEBUG_UTILS_MESSAGE_TYPE_DEVICE_ADDRESS_BINDING_BIT_EXT,
+                  .pfnUserCallback = sve4_log_vulkan_callback,
+              },
           .pApplicationInfo =
               &(VkApplicationInfo){
                   .sType = VK_STRUCTURE_TYPE_APPLICATION_INFO,
